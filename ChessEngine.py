d13cc503121eca825cbb6512bb069b095adf1604
Nolan Reilly <nolanreilly02@gmail.com>
2024-06-14 14:49:20 -0500

    Fix capture bug for h file

--- ChessEngine.py
+++ ChessEngine.py
@@ -28,7 +28,7 @@ class Move():
                     "5": 3, "6": 2, "7": 1, "8": 0 }
     rowsToRanks = { v: k for k, v in ranksToRows.items() }
     filesToCols = { "a": 0, "b": 1, "c": 2, "d": 3,
-                    "e": 4, "f": 5, "g": 6, "h": 6 }
+                    "e": 4, "f": 5, "g": 6, "h": 7 }
     colsToFiles = { v: k for k, v in filesToCols.items() }
 
     def __init__(self, startSq, endSq, board):
